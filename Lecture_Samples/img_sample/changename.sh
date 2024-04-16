075c7770d30e63f5a9d0b0a7df8b732413951a09
alex010204 <32715602-alex010204@users.noreply.replit.com>
2024-04-16 07:48:15 +0000

    yes

--- Lecture_Samples/img_sample/changename.sh
+++ Lecture_Samples/img_sample/changename.sh
@@ -1,4 +1,4 @@
 for file in image.jpg.*; do
-  new_name=$(echo "$file" | sed 's/image\.jpg\.//')
-  mv "$file" "image$new_name.jpg"
+  newName=$(echo "$file" | sed 's/image\.jpg\.//')
+  mv $file "image$newName.jpg"
 done
